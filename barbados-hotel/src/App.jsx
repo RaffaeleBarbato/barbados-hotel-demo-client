9004372b62b9020a2837538a80a0397df9cbafde
RaffaeleBarbato <raffaele.barbato1997@gmail.com>
2024-02-21 18:42:33 +0100

    first draft demo-client

---
+++ barbados-hotel/src/App.jsx
@@ -0,0 +1,14 @@
+import React  from "react"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import AddRoom from "./components/room/AddRoom"
+
+function App() {
+  return (
+  <>
+  <AddRoom/>
+  </>
+  )
+}
+
+export default App
