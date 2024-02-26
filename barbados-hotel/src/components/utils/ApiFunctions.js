ea6b0dc65ee00a89c48d585d6feaa8d07e885bb9
RaffaeleBarbato <raffaele.barbato1997@gmail.com>
2024-02-26 17:22:27 +0100

    Added UpdateRoom function

--- barbados-hotel/src/components/utils/ApiFunctions.js
+++ barbados-hotel/src/components/utils/ApiFunctions.js
@@ -49,3 +49,22 @@ export async function deleteRoom(roomId) {
 	}
 }
 
+/* This function updates a room */
+export async function updateRoom(roomId, roomData) {
+    const formData = new FormData()
+    formData.append("roomType", roomData.roomType)
+    formData.append("roomPrice", roomData.roomPrice)
+    formData.append("photo", roomData.photo)
+    const response = await api.put(`/rooms/update/${roomId}`, formData)
+    return response
+}
+
+/* This function gets a room by the id */
+export async function getRoomById(roomId) {
+    try{
+        const result = await api.get(`/rooms/room/${roomId}`)
+        return result.data
+    } catch(error){
+        throw new Error(`Error fetching room ${error.message}`)
+    }
+}
