c17da1316ebe3ca8dbe856ec067cc79964ce86d6
RaffaeleBarbato <raffaele.barbato1997@gmail.com>
2024-02-28 10:48:45 +0100

    Added components to display existing rooms

--- barbados-hotel/src/App.jsx
+++ barbados-hotel/src/App.jsx
@@ -8,7 +8,7 @@ import Home from "./components/home/Home"
 import EditRoom from "./components/room/EditRoom"
 import Navbar from "./components/layout/NavBar"
 import Footer from "./components/layout/Footer"
-
+import RoomListing from "./components/room/RoomListing"
 
 function App() {
   return (
@@ -21,6 +21,7 @@ function App() {
           <Route path="/edit-room/:roomId" element={<EditRoom/>}/>
           <Route path="/existing-rooms" element={<ExistingRooms/>}/>
           <Route path="/add-room" element={<AddRoom/>}/>
+          <Route path="/browse-all-rooms" element={<RoomListing/>}/>
         </Routes>
       </Router>
       <Footer/>
