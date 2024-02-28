3232b16b47341b42350bf3cca5bdad1b8ceb1217
RaffaeleBarbato <raffaele.barbato1997@gmail.com>
2024-02-28 17:15:16 +0100

    Added RoomCarousel to homepage

--- barbados-hotel/src/components/home/Home.jsx
+++ barbados-hotel/src/components/home/Home.jsx
@@ -2,15 +2,19 @@ import React, { useContext } from "react"
 import MainHeader from "../layout/MainHeader"
 import HotelService from "../common/HotelService"
 import Parallax from "../common/Parallax"
+import RoomCarousel from "../common/RoomCarousel"
 
 const Home = () => {
 	return (
 		<section>
 			<MainHeader />
 			<div className="container">
+				<RoomCarousel/>
 				<Parallax />
+				<RoomCarousel/>
 				<HotelService />
 				<Parallax />
+				<RoomCarousel/>
 			</div>
 		</section>
 	)
